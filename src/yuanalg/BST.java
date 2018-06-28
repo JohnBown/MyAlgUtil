625485baa30e50f5165c49868d28b6dbb68f4d16
JohnBown <yuanchaoradom@gmail.com>
2018-06-28 16:28:11 +0800

    更新了BST.java

--- src/yuanalg/BST.java
+++ src/yuanalg/BST.java
@@ -3,7 +3,7 @@ package yuanalg;
 public abstract class BST<E extends Comparable<E>> extends BinTree<E>{
 
     /**
-     *
+     * 二叉搜索树查找
      * @param e
      * @return
      */
@@ -20,9 +20,59 @@ public abstract class BST<E extends Comparable<E>> extends BinTree<E>{
         return searchIn((e.compareTo((E)v.data)<0?v.lc:v.rc),e,hot);
     }
 
-    abstract BinNode insert(E e);//插入
+    /**
+     * 二叉搜索树插入
+     * @param e
+     * @return
+     */
+    BinNode insert(E e) {//插入
+        BinNode x = search(e);//查找目标（留意hot的设置）
+        if (x==null){//即禁止雷同元素，故仅在查找失败时才实施插入操作
+            x = new BinNode(e,hot);//在x处创建新节点，以hot为父亲
+            size++; updateHeightAbove(x);//更新全树规模，更新x及其历代祖先的高度
+        }
+        return x;//无论e是否存在于原树，至此总有x.data == e
+    }
 
-    abstract boolean remove(E e);//删除
+    /**
+     * 二叉搜索树删除
+     * @param e
+     * @return
+     */
+    boolean remove(E e) {//删除
+        BinNode x = search(e);//查找目标（留意hot的设置）
+        if (x==null) return false;//确认目标存在（此时hot为x的父亲）
+        removeAt(x, hot);//分两大类情况实施删除，更新全树规模
+        size--;//更新全树规模
+        updateHeightAbove(hot);//更新hot及其历代祖先的高度
+        return true;
+    }//删除与否由返回值指示
+
+    BinNode removeAt(BinNode x, BinNode hot){
+        BinNode w = x;//实际被摘除的节点，初值同x
+        BinNode succ = null;//实际被删除节点的接替者
+        if (x.lc==null)//若*x的左子树为空，则可
+            succ = x = x.rc;//直接将*x替换为其右子树
+        else if (x.rc==null)//若右子树为空，则可
+            succ = x = x.lc;//对称地处理——注意：此时succ != NULL
+        else {//若左右子树均存在，则选择x的直接后继作为实际被摘除节点，为此需要
+            w = w.succ();//（在右子树中）找到*x的直接后继*w
+            E temp = (E)w.data;//交换*x和*w的数据元素
+            w.data = x.data;
+            x.data = temp;
+            BinNode u = w.parent;
+            if (u==x){//隔离节点*w
+                u.rc = succ = w.rc;
+            }else {
+                u.lc = succ = w.rc;
+            }
+        }
+        hot = w.parent; //记录实际被删除节点的父亲
+        if (succ!=null) succ.parent = hot;//并将被删除节点的接替者与hot相联
+        return succ;//释放被摘除节点，返回接替者
+    }
+
+
 
     BinNode hot;//命中节点的父亲
 
