dd285be58b2c3e86ad6ba1c799feabc6add9719b
JohnBown <yuanchaoradom@gmail.com>
2018-07-03 15:09:14 +0800

    更新了BTree.java

--- src/yuanalg/BTree.java
+++ src/yuanalg/BTree.java
@@ -26,8 +26,8 @@ public class BTree<E extends Comparable<E>> {
     int size; int order; BTNode root;//关键码总数、阶次、根
     BTNode hot;//search()最后访问的非空节点位置
 
-    void solveOverflow(BTNode t);//因插入而上溢后的分裂处理
-    void solveUnderflow(BTNode t);//因删除而下溢后的合并处理
+    void solveOverflow(BTNode t){}//因插入而上溢后的分裂处理
+    void solveUnderflow(BTNode t){}//因删除而下溢后的合并处理
 
     /**
      *
@@ -44,6 +44,26 @@ public class BTree<E extends Comparable<E>> {
         return null;//失败
     }
 
-    boolean insert(E e);//插入
-    boolean remove(E e);//删除
+    boolean insert(E e) {//插入
+        BTNode v = search(e);
+        if (v==null) return false;//确认e不存在
+        int r = hot.key.indexOf(e);//在节点hot中确定插入位置
+        hot.key.add(r+1,e);//将新关键码插至对应的位置
+        hot.child.add(r+2,null);//创建一个空子树指针
+        size++;solveOverflow(hot);//如果发生上溢，需做分裂
+        return true;//插入成功
+    }
+
+    boolean remove(E e) {//删除
+        BTNode v = search(e);
+        if (v==null) return false;//确认e存在
+        int r = v.key.indexOf(e);//确定e在v中的秩
+        if (v.child!=null){//若v非叶子
+            BTNode u = (BTNode) v.child.elementAt(r+1);//在右子树中一直存在，即可
+            while (u.child!=null) u = (BTNode) u.child.elementAt(0);//找到e的后继（必属于某叶节点）
+            v.key.add(r,u.key.get(0));v=u;r=0;//并与之交换位置
+        }//至此，v必然位于最底层，且其中第r个关键码就是待删除者
+        v.key.remove(r);v.child.remove(r+1);size--;
+        solveUnderflow(v);return true;//如有必要，需做旋转或合并
+    }
 }
